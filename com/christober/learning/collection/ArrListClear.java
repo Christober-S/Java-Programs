0074045a2c22cda7b3b53b43086c590ee3b3b5c9
Christober-S <christobersg@gmail.com>
2025-10-10 14:21:33 +0530

    created pgm for list

---
+++ com/christober/learning/collection/ArrListClear.java
@@ -0,0 +1,20 @@
+package learning.collection;
+
+import java.util.*;
+
+public class ArrListClear {
+    static void main() {
+        // Clear method is used to remove all the elements from the list
+        // and it returns void
+        // After calling clear method the size of the list will be 0
+        // and the list will be empty
+        // Example:
+         List<Integer> list = new ArrayList<>();
+         list.add(100);
+         list.add(200);
+         list.add(300);
+         System.out.println("Size before clear: " + list.size());
+         list.clear();
+         System.out.println("Size after clear: " + list.size());
+    }
+}
---
+++ com/christober/learning/collection/ListPgm.java
@@ -0,0 +1,23 @@
+package learning.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPgm {
+    static void main() {
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(0, 100);
+        list1.add(1, 200);
+        list1.add(2, 300);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(0, 400);
+        list2.add(1, 500);
+        list2.add(2, 600);
+
+        list1.addAll(2, list2);
+        list1.forEach((Integer val) -> System.out.println(val));
+
+
+    }
+}
